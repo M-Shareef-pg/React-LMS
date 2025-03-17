2404042b0c8d10d0354360b1e474fa5608c3aaa8
M-Shareef-pg <najmussaqibpro@gmail.com>
2025-03-17 16:38:43 +0500

    message

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -5,10 +5,30 @@ import Card from "./components/card";
 function App() {
   return (
     <div className="parent-card-div">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        title="OWL"
+        desc="Silent watcher of the midnight sky,
+Wisdom gleams within its golden eye."
+        img="src/assets/one.jpg"
+      />
+      <Card
+        title="CROW"
+        desc="Dark-winged shadow, sharp and sly,
+A voice that echoes through the sky."
+        img="src/assets/four.png"
+      />
+      <Card
+        title="EAGLE"
+        desc="Majestic hunter, fierce and free,
+Ruler of the sky and sea."
+        img="src/assets/two.jpg"
+      />
+      <Card
+        title="PEGION"
+        desc="Gentle wings in peaceful flight,
+A symbol of love in soft moonlight."
+        img="src/assets/three.jpg"
+      />
     </div>
   );
 }
--- vite-react/src/components/card.jsx
+++ vite-react/src/components/card.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import "./card.css";
 
-function Card() {
+function Card(props) {
   return (
     <div className="mainParent">
       {/* upper card part */}
       <div className="upper-part-wrapper">
-        <img src="src/assets/one.jpg" alt="" />
-        <h1>OWL</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          expedita libero minus, dignissimos porro laudantium sequi sit{" "}
-        </p>
+        <div className="img-wrapper"></div>
+      </div>
 
-        {/* lower part started */}
-        <div className="lower-part-wrapper">
-          <div className="flex-child">
-            <h3>Lifespan</h3>
-            <p>40-50 years</p>
-          </div>
+      <div className="mid-part-wrapper">
+        <img src={props.img} alt="image description" />
+        <h1>{props.title}</h1>
+        <p>{props.desc}</p>
+      </div>
 
-          {/* 2nd child */}
-          <div className="flex-child">
-            <h3>Mass</h3>
-            <p>4-6 kg</p>
-          </div>
+      {/* lower part started */}
+      <div className="lower-part-wrapper">
+        <div className="flex-child">
+          <h3>Lifespan</h3>
+          <p>40-50 years</p>
+        </div>
 
-          {/* 3rd child */}
-          <div className="flex-child">
-            <h3>Length</h3>
-            <p>1-3 m</p>
-          </div>
+        {/* 2nd child */}
+        <div className="flex-child">
+          <h3>Mass</h3>
+          <p>4-6 kg</p>
+        </div>
+
+        {/* 3rd child */}
+        <div className="flex-child">
+          <h3>Length</h3>
+          <p>1-3 m</p>
         </div>
       </div>
     </div>
