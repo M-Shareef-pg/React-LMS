65037358f35accb0408a93abb9cfbd4d26d54210
M-Shareef-pg <najmussaqibpro@gmail.com>
2025-03-21 23:22:52 +0500

    message

--- color/src/App.jsx
+++ color/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
+import Button from "./components/button";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("#A6A6A8");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="body-parent" style={{ backgroundColor: color }}>
+      <div className="navbar">
+        <h1>Color Picker</h1>
+        <div>
+          <Button title="RED" onClick={() => setColor("red")} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
